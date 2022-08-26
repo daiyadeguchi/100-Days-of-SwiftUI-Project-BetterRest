3210106b900d071dc4cef4ff3ee251514223e701
daiyadeguchi <daiyadeguchi@gmail.com>
2022-08-27 07:50:51 +0900

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Daiya Deguchi on 2022/08/27.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
